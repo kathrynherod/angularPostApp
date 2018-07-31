2623ea1c6c57c3a5f4e8f89da434ea2d0ce3bffe
Herod, Kathryn <codingkat7@gmail.com>
2018-07-31 12:20:24 -0500

    set up firebase and package

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { AngularFireModule } from 'angularfire2';
+import { environment } from './../environments/environment';
 
 @NgModule({
   declarations: [AppComponent, NavbarComponent],
-  imports: [BrowserModule, RouterModule],
+  imports: [
+    BrowserModule,
+    RouterModule,
+    AngularFireModule.initializeApp(environment.firebase)
+  ],
   providers: [],
   bootstrap: [AppComponent]
 })
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyAXzHzF_UhXqpdE1oV1qTrZsUekwqXJ_E8',
+    authDomain: 'angulargrocery-cf3d9.firebaseapp.com',
+    databaseURL: 'https://angulargrocery-cf3d9.firebaseio.com',
+    projectId: 'angulargrocery-cf3d9',
+    storageBucket: 'angulargrocery-cf3d9.appspot.com',
+    messagingSenderId: '253615274342'
+  }
 };
