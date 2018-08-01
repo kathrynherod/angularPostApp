2ec69c08407b81c741c8e8e49fdec501246b9f02
Herod, Kathryn <codingkat7@gmail.com>
2018-08-01 13:35:44 -0500

    add form validation

--- src/app/posts/post-create/post-create.component.ts
+++ src/app/posts/post-create/post-create.component.ts
@@ -1,6 +1,6 @@
 import { Component, EventEmitter, Output } from '@angular/core';
 import { Post } from '../post.model';
-
+import { NgForm } from '@angular/forms';
 @Component({
   selector: 'app-post-create',
   templateUrl: './post-create.component.html',
@@ -10,10 +10,13 @@ export class PostCreateComponent {
   enteredTitle;
   enteredContent;
   @Output() postAdded = new EventEmitter<Post>();
-  onAddPost() {
+
+  onAddPost(form: NgForm) {
+    //if the form is invalid/blank, dont actually submit it
+    if (form.invalid) return;
     const post: Post = {
-      title: this.enteredTitle,
-      content: this.enteredContent
+      title: form.value.title,
+      content: form.value.content
     };
     this.postAdded.emit(post);
   }
